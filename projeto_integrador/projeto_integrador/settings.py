74d9052decade834ece142925fcb21ed4c20569f
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2023-04-30 23:22:27 -0300

    back

--- projeto_integrador/projeto_integrador/settings.py
+++ projeto_integrador/projeto_integrador/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projeto_integrador.registro_de_animal',
+    'registro_de_animal',
 ]
 
 MIDDLEWARE = [
