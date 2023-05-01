83b4bcb2fe8d2504443dbd6a1370e233a92a3dd0
PAULO HENRIQUE DA SILVA NETO <86234861+PauloHSNeto@users.noreply.github.com>
2023-04-30 23:13:31 -0300

    folder

--- projeto_integrador/projeto_integrador/wsgi.py
+++ projeto_integrador/projeto_integrador/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_integrador.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_integrador.projeto_integrador.settings')
 
 application = get_wsgi_application()
